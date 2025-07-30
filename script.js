8dda68bff78431c980ba8bbfb4d7726187eb44a0
nblj.git 🍁 <noblejosephalex@gmail.com>
2025-07-31 01:38:05 +0400

    Polish up some edges.

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-let isToggled = false; 
-document.querySelector(".profile-picture").style.display = "hidden";
+let isToggled = false;
+const profilePic = document.querySelector(".profile-picture");
+
+if (profilePic) {
+  profilePic.style.visibility = "hidden";
+}
 
 function flipCard() {
   if (isToggled) {
-    document.getElementById("bio").innerHTML = "learning web development; python.";
-    document.querySelector(".profile-picture").style.visibility = "hidden";
+    document.getElementById("bio").innerHTML =
+      "learning web development; python.";
+
+    if (profilePic) {
+      profilePic.style.visibility = "hidden";
+    }
+
     document.getElementById("links").innerHTML = `
         <a style="font-size: 1.3rem;">⤷</a><a href="https://github.com/noblemage">github</a> <br>
         <a style="font-size: 1.3rem;">⤷</a><a href="https://instagram.com/nblj.ig">instagram</a> <br>
         <a style="font-size: 1.3rem;">⤷</a><a href="discordapp.com/users/880789514723950652">discord</a> <br>
-    `
+    `;
+
     isToggled = false;
   } else {
     document.getElementById("bio").innerHTML = "";
-    document.querySelector(".profile-picture").style.visibility = "visible";
+
+    if (profilePic) {
+      profilePic.style.visibility = "visible";
+    }
+
     document.getElementById("links").innerHTML = `
-    building simple, minimal experiences.
-    <ul>
-        <li><a href="#">placeholder</a></li>
-    </ul>
+        building simple, minimal experiences.
+        <ul>
+            <li><a href="#">placeholder</a></li>
+        </ul>
     `;
+
     isToggled = true;
   }
 }
